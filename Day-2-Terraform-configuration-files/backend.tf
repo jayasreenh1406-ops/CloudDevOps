e737bdbae5f6096f65c7ec25ab57c7edaa529dac
jayasreenh1406-ops <jayasreenh1406@gmail.com>
2025-12-04 16:20:24 +0530

    Update backend.tf

--- Day-2-Terraform-configuration-files/backend.tf
+++ Day-2-Terraform-configuration-files/backend.tf
@@ -1,7 +1,7 @@
 terraform {
-  backend "s3" {
-    bucket = "hgcghxhgxsxcshxsxs"
-    key    = "day-2/terraform.tfstate"
-    region = "us-east-1"
+ # backend "s3" {
+ #   bucket = "hgcghxhgxsxcshxsxs"
+ #   key    = "day-2/terraform.tfstate"
+  #  region = "us-east-1"
   }
 }
